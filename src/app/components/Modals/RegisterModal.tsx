f1b1c77f37742397f8e6d959cf976742dfa0f0c3
Ben Santos <imbensantos@gmail.com>
2023-10-12 15:50:43 +0800

    Bug: Added login flow after signup

--- src/app/components/Modals/RegisterModal.tsx
+++ src/app/components/Modals/RegisterModal.tsx
@@ -32,7 +32,11 @@ const RegisterModal = () => {
     setIsLoading(true)
 
     axios.post('/api/register', data)
-      .then(() => { registerModal.onClose() })
+      .then(() => { 
+        toast.success('Successfully Registered!')
+        registerModal.onClose()
+        loginModal.onOpen()
+      })
       .catch((error) => { toast.error("Something went wrong") })
       .finally(() => { setIsLoading(false) })
   }
