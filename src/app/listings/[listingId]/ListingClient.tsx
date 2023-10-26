8decd0397ffcabdd8526fc1f50c674bdbe3ba479
Ben Santos <imbensantos@gmail.com>
2023-10-26 23:02:39 +0800

    Bug: Fixed wrong url push when booking a reservation

--- src/app/listings/[listingId]/ListingClient.tsx
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -72,7 +72,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser, res
       .then(() => {
         toast.success('Listing reserved!')
         setDateRange(initialDateRange)
-        router.push('/trips')
+        router.push('/flight-paths')
       })
       .catch(() => { toast.error('Something went wrong.') })
       .finally(() => { setIsLoading(false) })
