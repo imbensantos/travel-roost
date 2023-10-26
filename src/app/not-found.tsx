2968dd0e61eaadedd291977890034470bdbe5614
Ben Santos <imbensantos@gmail.com>
2023-10-26 21:24:01 +0800

    Feat: Added 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import EmptyState from '@components/EmptyState'
+
+const NotFound = () => {
+  return (
+    <EmptyState 
+      title="404"
+      subtitle="This page could not be found."
+    />
+  )
+}
+
+export default NotFound
